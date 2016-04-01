ce0ff353d1e6d63fd35f46533ed3319f81a9572d
lusin-ss <lusin.ss@outlook.com>
2016-04-01 17:00:59 +0300

    jango

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <meta http-equiv="Content-type" content="text/html; charset=utf-8">
 <meta content="" name="description"/>
 <meta content="" name="author"/>
-<!-- BEGIN GLOBAL MANDATORY STYLES 
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
 <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700&amp;subset=all' rel='stylesheet' type='text/css'>
- -->
+ 
 <link href="assets/plugins/socicon/socicon.css" rel="stylesheet" type="text/css"/>
 <link href="assets/plugins/bootstrap-social/bootstrap-social.css" rel="stylesheet" type="text/css"/>
 <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
